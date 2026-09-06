8f1821aab3889a90b795b170f91c3c71b4fb0f13
masiiha <m.hajalian@gmail.com>
2026-09-07 01:31:10 +0330

    hajalian file is updated

--- Hajalian.swift
+++ Hajalian.swift
@@ -6,17 +6,17 @@
 //
 
 
-//۱=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//یک فولدری هست به نام
-//assets
-//باید همه تصاویر در اون قرار بگیره
-//۲=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//یک فولدری ساختی به نام
-//models
-//توی این فولدر باید اگر مدلی داره بزاری ولی الان اینی که ساخنی ویو مدل هست اگر فعلا نمیدونی مدل چیه مهم نیست بعدا یا میگیرمش
-//۳=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//دقیقا بعد اینکه فهمیدی
-//NSLayoutConstraint
-//این کاربردش چیه تو یتلگرام یا همینجا برام ی توضیجی ازش بنویس یا مثال واقعی برای کاربردش بیار
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// toye appicon nabayd ax bezarid onja baraye icon app hast k sharayete khodesh dare
+
+//ax haroo be soorat kilooii vasate poshe assets narizid poshe besazid baraye har controller k badan bedoonid ki kojast
+// toye file view controller khate 23 az clipsToBounds estefade kardi y khati begoo kariiesh chie
+
+// line 20 searchButton.frame.size.width = 120 ino darim nabayad in moodeli azash estefade konim
+
+//khate 18, 19  searchButton.setTitle("show me...", for:.normal) searchButton.setTitle("Loading...", for: .highlighted) on ghesmate highlighted, normal va bagheye item haii k dare ro check kon va begoo yani chi va karbordesho begoo
+//UITextFieldDelegate in midooni chie? va khate 16 roo baraye chi neveshti agar 2 ta text dashte bashim chi mishe?
+
+// lotfan toye ui hich chizi roo manoot b copy paste estefade nakon agar nemidooni chie biar ba ham halesh konim agar midooni azash estefade kon
+
+//shoma bayad toye ui hameye mavaredi k minevisi ya estefade mikoni ro dark koni choon mohemtarin jaii k bayad yad begiri hamine
 
